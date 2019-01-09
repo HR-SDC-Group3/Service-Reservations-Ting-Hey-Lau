38b68af6b5e6a3053ce7cc9b30fbf26c007f0199
michael lau <michael99940@gmail.com>
2019-01-08 17:22:33 -0800

    data generation function tested and functions as intended

---
+++ db/dataGenerator.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const faker = require('faker');
+const path = require('path');
+
+const wstream = fs.createWriteStream(__dirname + '/data.txt');
+let i = 0;
+
+const WriteOne = () => {
+	while (i < 10000000){
+		var res = [];
+		for (var j = 0; j < Math.floor(Math.random()*5); j++) {
+			var date = faker.date.past();
+			res.push({
+				id: j,
+				dateToReserve: date.getDay().toString() + date.getMonth().toString() + date.getFullYear().toString(),
+				timeToReserve: date.getHours().toString() + date.getMinutes().toString(), 
+				partySize: Math.floor(Math.random()*5).toString(),
+			})
+			var entry = {
+				id: i,
+				name: faker.company.companyName(),
+				reservations: res,
+			};
+			if(!wstream.write(JSON.stringify(entry))) {
+				return;
+			}
+			i++;
+		}
+	}
+	wstream.end();
+};
+
+wstream.on('drain', () => {
+	WriteOne();
+});
+
+WriteOne();
