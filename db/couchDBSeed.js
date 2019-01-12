7ed6881068daf2d2f6e7d2074a391626b5c5333d
michael lau <michael99940@gmail.com>
2019-01-11 19:44:20 -0800

    seeding + json generation script complete at a fundamental level

--- db/couchDBSeed.js
+++ db/couchDBSeed.js
@@ -1,50 +1,73 @@
 const auth = require('./couchConfig.js');
 const nano = require('nano')(`http://${auth.user}:${auth.pass}@localhost:5984`);
 const path = require('path');
+const jsonlines = require('jsonlines');
 const Promise = require('bluebird');
 const fs = require('fs');
-console.log(path.join(__dirname, 'restaurants.csv'));
-const rRestStream = fs.createReadStream(path.join(__dirname, 'restaurants.csv'));
-const rResStream = fs.createReadStream(path.join(__dirname, 'reservations.csv'));
+const parser = jsonlines.parse();
+const rStream = fs.createReadStream(path.join(__dirname, 'data2.jsonl'));
+const opts = {url:`http://${auth.user}:${auth.pass}@localhost:5984`, database:'opentablereservations', type: "jsonl", delimiter:'\n'};
 
+let check = false;
 
 async function seeder() {
-	await nano.db.destroy('opentablereservations');
-	await nano.db.create('opentablereservations');
-	const restaurants = await nano.use('opentablereservations');
-	const iRestStream = restaurants.attachment.insertAsStream('restaurants', 'restaurants.csv', null, 'text/csv', { rev: '12-150985a725ec88be471921a54ce91452'})
-	  .on('end', (body) => {
-	    console.log('done inserting restaurants', body);
-	});
-	await rRestStream.pipe(iRestStream);
-	const iResStream = restaurants.attachment.insertAsStream('reservations', 'reservations.csv', null, 'text/csv', { rev: '12-150985a725ec88be471921a54ce91452'})
-	  .on('end', (body) => {
-	    console.log('done inserting reservations', body);
-	});
 
-	await rResStream.pipe(iResStream);
-  // await restaurants.insert({omegalul: '4head'}, 'memes').then((body) => {
-  // 	console.log(body);
-  // });
-  // await restaurants.find({selector:{_id: {'$eq': 'memes'}}}).then((doc) => {
-  // 	console.log(doc);
-  // });
+	await nano.db.list().then(body => {
+		body.forEach((name) => {
+			if (name === 'opentablereservations') {
+				check = true;
+				nano.db.destroy(name).then(() => {
+					nano.db.create('opentablereservations').then(() => {
+						const database = nano.db.use('opentablereservations');
+						rStream.pipe(parser);
+						parser.on('data', data => {
+							database.insert({
+								restaurantname: data.restaurantname,
+								reservations: data.reservations
+							}, data.id.toString(), (err, res) => {
+								if (err) {
+									console.log(err);
+								} else {
+									console.log(res);
+								}
+							})
+						});
+						parser.on('end', () => {
+							console.log('no more data');
+						});
+						rStream.on('end', () => {
+							parser.end();
+						})
+					})
+				})
+			}
+		});
+		if (!check) {
+			nano.db.create('opentablereservations').then(() => {
+				const database = nano.db.use('opentablereservations');
+				rStream.pipe(parser);
+				parser.on('data', data => {
+					database.insert({
+						restaurantname: data.restaurantname,
+						reservations: data.reservations
+					}, data.id.toString(), (err, res) => {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log(res);
+						}
+					})
+				});
+				parser.on('end', () => {
+					console.log('no more data');
+				});
+				rStream.on('end', () => {
+					parser.end();
+				});
+			});	
+		}	
+	});
 }
 
 seeder();
 
-// nano.db.create('opentablereservations').then(() => {
-// 	console.log('opebtablereservations db created poggers');
-// 	nano.use('opentablereservations')
-// }, err => {
-// 	console.log('error route');
-// 	if (err) {
-// 		nano.db.destroy('opentablereservations').then(() => {
-// 			nano.db.create('opentablereservations').then(() => {
-// 				console.log('opebtablereservations db created poggers');
-// 			}, err => {
-// 				console.log('error occured ', err);
-// 			})
-// 		})
-// 	} 	
-// });
